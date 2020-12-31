bbb0e7a2c02ee49debc43b445548ea8a8086d016
rtang03 <7686266+rtang03@users.noreply.github.com>
2021-01-01 02:32:23 +0800

    add typescript-eslint-jest

--- app1/next.config.js
+++ app1/next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
       config.node = {
-        fs: 'empty'
+        fs: 'empty',
       };
     }
     return config;
